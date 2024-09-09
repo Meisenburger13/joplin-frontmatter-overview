99341ca8ab89e6025873bdf5a46bf9ccc097ab08
Meisenburger13 <melissa.eisenburger@gmail.com>
2024-09-09 22:40:37 +0200

    init

---
+++ src/frontmatterOverview.ts
@@ -0,0 +1,35 @@
+export default function() {
+	return {
+		plugin: function(markdownIt, _options) {
+			const defaultRender = markdownIt.renderer.rules.fence || function(tokens, idx, options, env, self) {
+				return self.renderToken(tokens, idx, options, env, self);
+			};
+
+			markdownIt.renderer.rules.fence = function (tokens, idx, options, env, self) {
+				const token = tokens[idx];
+				if (token.info !== 'frontmatter-overview') return defaultRender(tokens, idx, options, env, self);
+
+				// Rich text editor support:
+				// The joplin-editable and joplin-source CSS classes mark the generated div
+				// as a region that needs special processing when converting back to markdown.
+				// This element helps Joplin reconstruct the original markdown.
+				const richTextEditorMetadata = `
+					<pre
+						class="joplin-source"
+						data-joplin-language="frontmatter-overview"
+						data-joplin-source-open="\`\`\`frontmatter-overview\n"
+						data-joplin-source-close="\`\`\`"
+					>${markdownIt.utils.escapeHtml(token.content)}</pre>
+				`;
+
+				return `
+					<div class="frontmatter-overview joplin-editable">
+						${richTextEditorMetadata}
+
+						<p>Frontmatter overview</p>
+					</div>
+				`;
+			};
+		},
+	}
+}
---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import joplin from 'api';
+import {ContentScriptType} from 'api/types';
+
+joplin.plugins.register({
+	onStart: async function() {
+		console.log("frontmatter overview started!")
+		await joplin.contentScripts.register(
+			ContentScriptType.MarkdownItPlugin,
+			'frontmatter-overview',
+			'./frontmatterOverview.js'
+		);
+	},
+});
