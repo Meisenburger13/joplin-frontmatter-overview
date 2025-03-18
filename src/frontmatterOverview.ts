1a1dbee0ca93d7859a4458f71658fd28abba4981
Meisenburger13 <melissa.eisenburger@gmail.com>
2025-03-18 14:28:29 +0100

    add renderContent function

--- src/frontmatterOverview.ts
+++ src/frontmatterOverview.ts
@@ -9,26 +9,29 @@ export default function() {
 				const token = tokens[idx];
 				if (token.info !== 'frontmatter-overview') return defaultRender(tokens, idx, options, env, self);
 
-				// Rich text editor support:
-				// The joplin-editable and joplin-source CSS classes mark the generated div
-				// as a region that needs special processing when converting back to markdown.
-				// This element helps Joplin reconstruct the original markdown.
-				const richTextEditorMetadata = `
-					<pre
-						class="joplin-source"
-						data-joplin-language="frontmatter-overview"
-						data-joplin-source-open="\`\`\`frontmatter-overview\n"
-						data-joplin-source-close="\`\`\`"
-					>${markdownIt.utils.escapeHtml(token.content)}</pre>
-				`;
+				function renderContent(message) {
+					// Rich text editor support:
+					// The joplin-editable and joplin-source CSS classes mark the generated div
+					// as a region that needs special processing when converting back to markdown.
+					// This element helps Joplin reconstruct the original markdown.
+					const richTextEditorMetadata = `
+						<pre
+							class="joplin-source"
+							data-joplin-language="frontmatter-overview"
+							data-joplin-source-open="\`\`\`frontmatter-overview\n"
+							data-joplin-source-close="\`\`\`"
+						>${markdownIt.utils.escapeHtml(token.content)}</pre>
+					`;
 
-				return `
-					<div class="frontmatter-overview joplin-editable">
-						${richTextEditorMetadata}
+					return `<div class="frontmatter-overview joplin-editable">
+								${richTextEditorMetadata}
+				
+								<p>${message}</p>
+							</div>
+						`;
+				}
 
-						<p>Frontmatter overview</p>
-					</div>
-				`;
+				return renderContent("Frontmatter Overview");
 			};
 		},
 	}
