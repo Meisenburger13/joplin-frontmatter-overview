2aaa628abb93cc64a6c6596255df8f31225511ca
Meisenburger13 <melissa.eisenburger@gmail.com>
2025-11-14 04:19:40 +0100

    refactor: use escapeHtml in linkHandler

--- src/services/linkHandler.ts
+++ src/services/linkHandler.ts
@@ -1,12 +1,12 @@
+import { escapeHtml } from "../utils";
+
 export function linksToHtml(frontmatter: any[]) {
 	for (const [key, value] of Object.entries(frontmatter)) {
 		if (typeof value !== "string") { continue; }
 
 		const links = value.matchAll(/\[(.*?)]\((.*?)\)/g);
 		for (const [mdLink, title, url] of links) {
-			const titleDiv = document.createElement("div");
-			titleDiv.textContent = title;
-			const htmlLink = `<a href="${url}">${titleDiv.innerHTML}</a>`;
+			const htmlLink = `<a href="${url}">${escapeHtml(title)}</a>`;
 			frontmatter[key] = frontmatter[key].replace(mdLink, htmlLink);
 		}
 	}
