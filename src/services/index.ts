e97dc9b0d5ab291d2ee7745204c557f54f797baa
Meisenburger13 <melissa.eisenburger@gmail.com>
2025-11-14 01:10:41 +0100

    refactor: makeTablesPermanent

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,6 +1,6 @@
-export * from "./commands";
 export * from "./frontmatter";
 export * from "./imageHandler";
 export * from "./linkHandler";
 export * from "./overview";
 export * from "./overviewSettings";
+export * from "./turndown";
---
+++ src/services/turndown.ts
@@ -0,0 +1,40 @@
+import joplin from "api";
+import TurndownService from "turndown";
+import { tables } from "turndown-plugin-gfm";
+import { renderOverview } from "../services";
+
+function imagesToMarkdownPlugin(service: TurndownService) {
+	service.addRule("imageToMarkdown", {
+		filter: "img",
+		replacement: function (_, node: HTMLElement) {
+			const alt = node.getAttribute("alt");
+			const id = node.getAttribute("data-resource-id");
+			return `![${alt}](:/${id})`;
+		}
+	});
+}
+
+const turndownService = (() => {
+	const service = new TurndownService();
+	service.use(tables);
+	service.use(imagesToMarkdownPlugin);
+	return service;
+})();
+
+export async function makeTablesPermanent() {
+	const selectedNote = await joplin.workspace.selectedNote();
+	if (!selectedNote) { return; }
+
+	let body:string = selectedNote.body;
+	const overviews = body.matchAll(/```frontmatter-overview\n([\s\S]*?)```/g);
+
+	// turn HTML tables to MD
+	for (const [frontmatterOverview, frontmatterSettings] of overviews) {
+		const htmlTable = await renderOverview(frontmatterSettings);
+		const markdownTable = turndownService.turndown(htmlTable);
+		body = body.replace(frontmatterOverview, markdownTable + "\n");
+	}
+
+	// update body
+	await joplin.data.put(["notes", selectedNote.id], null, { body: body });
+}
