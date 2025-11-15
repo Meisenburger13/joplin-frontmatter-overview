57568366907bb5dbe91d4f1be51f66ccb5d21be2
Meisenburger13 <melissa.eisenburger@gmail.com>
2025-11-16 00:42:27 +0100

    refactor: overview

--- src/services/imageHandler.ts
+++ src/services/imageHandler.ts
@@ -1,13 +1,12 @@
-import joplin from "api";
 import { escapeHtml } from "../utils";
 
-export async function imagesToHtml(frontmatter: any[], isMobile: boolean, width: any, height: any) {
+export async function imagesToHtml(frontmatter: any[], isMobile: boolean, width: any, height: any, getResourcePath: (id: string) => Promise<string>) {
 	for (const [key, value] of Object.entries(frontmatter)) {
 		if (typeof value !== "string") { continue; }
 
 		const images = value.matchAll(/!\[(.*?)]\(:\/([a-z0-9]{32})\)/g);
 		for (const [mdImage, alt, id] of images) {
-			const path = await joplin.data.resourcePath(id);
+			const path = await getResourcePath(id);
 			const src = isMobile ? "file:///" : "joplin-content://note-viewer/";
 			const htmlImage = `<img data-resource-id="${id}" src="${src}${path}" style="max-width: ${width}; max-height: ${height}" alt="${escapeHtml(alt)}">`;
 			frontmatter[key] = frontmatter[key].replace(mdImage, htmlImage);
--- src/services/overview.ts
+++ src/services/overview.ts
@@ -8,30 +8,44 @@ import {
 } from "../services";
 import { getNotes, isMobilePlatform, sortNotes } from "../utils";
 
-export async function renderOverview(overview:string) {
-	const pluginSettings = await joplin.settings.values(["width", "height"]);
+async function getResourcePath(id: string) {
+	return await joplin.data.resourcePath(id);
+}
+
+export async function renderOverview(overview: string) {
+	const { width, height } = await joplin.settings.values(["width", "height"]);
 	const isMobile = await isMobilePlatform();
+
 	const overviewSettings = getOverviewSettings(overview);
 	if (typeof overviewSettings === "string") { return overviewSettings; }
-	// get notes
+
 	let notes = await getNotes(overviewSettings.from);
+
+	// parse frontmatter
 	for (const note of notes) {
 		note.frontmatter = getFrontmatter(note.body);
 	}
-	// keep only notes with frontmatter if excludeEmpty
+
+	// filter empty?
 	if (overviewSettings.excludeEmpty) {
-		notes = notes.filter(i => Object.keys(i.frontmatter).length > 0);
-	}
-	// sort notes
-	notes.sort((a, b) => { return sortNotes(a, b, overviewSettings.sort) });
-	if (overviewSettings.reverseSort) {
-		notes = notes.reverse();
+		notes = notes.filter(note => Object.keys(note.frontmatter).length > 0);
 	}
 
+	// sort
+	notes.sort((a, b) => sortNotes(a, b, overviewSettings.sort));
+	if (overviewSettings.reverseSort) notes.reverse();
+
+	// transform notes
 	for (const note of notes) {
 		// convert images to html
-		note.frontmatter = await imagesToHtml(note.frontmatter, isMobile, pluginSettings["width"], pluginSettings["height"]);
-		// convert Markdown links to html
+		note.frontmatter = await imagesToHtml(
+			note.frontmatter,
+			isMobile,
+			width,
+			height,
+			getResourcePath
+		);
+		// convert links to html
 		note.frontmatter = linksToHtml(note.frontmatter);
 	}
 
