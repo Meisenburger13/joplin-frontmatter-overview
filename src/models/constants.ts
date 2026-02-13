9b42e2bec127c150c4c3e02e682b8abbc1f05a89
Meisenburger13 <melissa.eisenburger@gmail.com>
2026-02-13 18:38:06 +0100

    feat: add NUM_BACKLINKS property

--- src/models/constants.ts
+++ src/models/constants.ts
@@ -1,6 +1,7 @@
 // special properties
 export const LINE_NUM = "LINE_NUM";
 export const NOTE_LINK = "NOTE_LINK";
+export const NUM_BACKLINKS = "NUM_BACKLINKS";
 
 // parameters
 export const DESC_SUFFIX = " DESC";
--- src/services/makeTableHtml.ts
+++ src/services/makeTableHtml.ts
@@ -1,18 +1,33 @@
-import { LINE_NUM, NOTE_LINK } from "../models";
+import { LINE_NUM, NOTE_LINK, NUM_BACKLINKS } from "../models";
 import { escapeHtml } from "../utils";
+import joplin from "../../api";
 
-function getPropertyValue(note: any, property: any, line_number: number) {
+async function getPropertyValue(note: any, property: any, line_number: number) {
 	let value = note.frontmatter[property] || "";
 	if (property === NOTE_LINK) {
 		value = `<a href=":/${note.id}">${escapeHtml(note.title)}</a>`;
-	}
-	else if (property === LINE_NUM) {
+	} else if (property === LINE_NUM) {
 		value = (line_number).toString();
+	} else if (property === NUM_BACKLINKS) {
+		let number = 0;
+		let pageNum = 1;
+		let response: { items: any; has_more: boolean; };
+		do {
+			response = await joplin.data.get(["search"], {
+				query: note.id,
+				type: "note",
+				page: pageNum,
+				fields: "id"
+			});
+			number += response["items"].length;
+			pageNum++;
+		} while (response.has_more)
+		value = number.toString();
 	}
 	return value;
 }
 
-export function makeTableHtml(properties: any[], notes: any[]) {
+export async function makeTableHtml(properties: any[], notes: any[]) {
 	// make header with aliases
 	let tableHtml = "<table><thead><tr>";
 	for (const prop of properties) {
@@ -21,14 +36,15 @@ export function makeTableHtml(properties: any[], notes: any[]) {
 	tableHtml += "</tr></thead>";
 
 	// add one row per note
-	notes.forEach((note, index)  => {
+	for (const note of notes) {
+		const index = notes.indexOf(note);
 		tableHtml += "<tr>";
 		for (const prop of properties) {
-			const propValue = getPropertyValue(note, prop.original, index + 1);
+			const propValue = await getPropertyValue(note, prop.original, index + 1);
 			tableHtml += `<td> ${propValue} </td>`;
 		}
 		tableHtml += "</tr>";
-	});
+	}
 
 	// finish table
 	tableHtml += "</table>";
